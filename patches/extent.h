99a9285192c254b099bf594f7175320407469233
artsky <artsky@node-0.artsky-298454.rise-pg0.utah.cloudlab.us>
2026-04-06 16:43:38 -0600

    created extent header file

---
+++ patches/extent.h
@@ -0,0 +1,37 @@
+// This file is located in linux-5.15.0/include/linux/extent.h
+
+#ifndef _LINUX_EXTENTS_H
+#define _LINUX_EXTENTS_H
+
+#include <linux/types.h>
+#include <linux/list.h>
+#include <linux/rbtree.h>
+#include <linux/spinlock.h>
+
+struct extent_page {
+    phys_addr_t paddr;
+    struct list_head page_node; 
+}
+
+struct extent {
+    phys_addr_t paddr_start; 
+    phys_addr_t paddr_end; 
+    unsigned long id; 
+    unsigned long num_pages; 
+    struct list_head page_list;
+    struct rb_node node; 
+}
+
+struct extent_table {
+    struct rb_root root; 
+    spinlock_t s_lock; 
+    unsigned long next_id; 
+    unsigned long total_extents;
+}
+
+struct extent_table *extent_table_alloc(void);
+void extent_table_free(struct extent_table *et);
+int  extent_table_insert_page(struct extent_table *et, phys_addr_t phys);
+void extent_table_print_stats(struct extent_table *et, pid_t pid);
+
+#endif
